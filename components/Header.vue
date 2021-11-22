80b2a6e2dc3be7b0cc1c90ea4af227fc4c7ddc48
Ashlyn <lilyx13@tutanota.com>
2021-11-22 10:05:46 -0700

    added a working header component using image as props

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+<header>
+  <h1> {{ pageInfo.pageName }}</h1>
+  <nuxt-img :src="pageInfo.image" alt="" />
+</header>
+</template>
+<script>
+export default {
+  props: ['pageInfo']
+}
+</script>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,2 +1,41 @@
-<template lang="pug">
+<template>
+<div>
+  <Header :pageInfo="siteInfo" />
+  <header>
+    <h1>Tech {{ siteInfo.pageName }}</h1>
+  </header>
+  <footer>
+    <p>&copy; 2021 | {{ siteInfo.author }}</p>
+  </footer>
+</div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      siteInfo: {
+        pageName: "Blog",
+        author: "Ashlyn",
+        image: "images/excavation.jpg"
+      },
+      topics: [
+        {
+          title: "JavaScript",
+          description: "Articles about JS",
+          difficulty: "Intermediate"
+        },
+        {
+          title: "Linux",
+          description: "Articles about Fedora Linux",
+          difficulty: "Advanced"
+        },
+        {
+          title: "Design",
+          description: "UI/UX design, logo design, and css styles.",
+          difficulty: "Beginner"
+        }
+      ]
+    }
+  }
+}
+</script>
